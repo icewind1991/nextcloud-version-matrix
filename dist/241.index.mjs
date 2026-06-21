25ddaceda212510db542d41b1a98fd5c1bc424e7
David Dreschner <github-2017@dreschner.net>
2026-06-21 23:07:25 +0200

    feat: Update dependencies to latest version and refactor action to ES6 module
    Signed-off-by: David Dreschner <github-2017@dreschner.net>

---
+++ dist/241.index.mjs
@@ -0,0 +1,13 @@
+export const id = 241;
+export const ids = [241];
+export const modules = {
+
+/***/ 9241:
+/***/ ((module) => {
+
+module.exports = eval("require")("web-streams-polyfill/ponyfill/es2018");
+
+
+/***/ })
+
+};
